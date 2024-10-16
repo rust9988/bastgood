369e1fc4abf91d8f3dd8ac17d22222e949aba312
bixuzhuceyigegitzhagnhao <bixuzhuceyigegitzhagnhao@outlook.com>
2024-10-16 19:50:07 +0800

    home_page.dart

--- flutter/lib/mobile/pages/home_page.dart
+++ flutter/lib/mobile/pages/home_page.dart
@@ -48,13 +48,14 @@ class HomePageState extends State<HomePage> {
   void initPages() {
     _pages.clear();
     if (!bind.isIncomingOnly()) {
-      _pages.add(ConnectionPage(
-        appBarActions: [],
-      ));
+     // _pages.add(ConnectionPage(
+     //   appBarActions: [],
+     // ));
     }
     if (isAndroid && !bind.isOutgoingOnly()) {
       _chatPageTabIndex = _pages.length;
-      _pages.addAll([ChatPage(type: ChatPageType.mobileMain), ServerPage()]);
+    //  _pages.addAll([ChatPage(type: ChatPageType.mobileMain), ServerPage()]);
+       _pages.add(ServerPage());
     }
     _pages.add(SettingsPage());
   }
