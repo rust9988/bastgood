9d5e68b1be821ce83f87d202fd323cedab774636
bixuzhuceyigegitzhagnhao <bixuzhuceyigegitzhagnhao@outlook.com>
2024-10-25 20:01:52 +0800

    user_model.dart

--- flutter/lib/models/user_model.dart
+++ flutter/lib/models/user_model.dart
@@ -141,12 +141,12 @@ class UserModel {
       return false;
     }
     //把日期写到名字里 显示在前台
-    if(data['name']!=null && gFFI.userModel.userName.value.contains(data['name']))
+    if(data['name']!=null && gFFI.userModel.userName.value==data['name'])
     {   
       final expdate = data['expdate'];
       if (expdate != null) {
-         gFFI.userModel.userLogin.value = data['name'] + "_有效期:" + data['expdate'];
-         gFFI.userModel.userName.value = data['name'] + "_有效期:" + data['expdate'];
+         gFFI.userModel.userLogin.value = "用户名:" + data['name'] + ",有效期:" + data['expdate'];
+         //gFFI.userModel.userName.value = data['name'] + "_有效期:" + data['expdate'];
       }
       return true;
     }
