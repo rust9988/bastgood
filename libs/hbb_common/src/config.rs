cfafc9d912bb76f97a61687dbd22ced365ae0e29
rust9988 <rustdesk9988@proton.me>
2024-11-23 02:13:15 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["110.42.44.109"];
-pub const PUBLIC_RS_PUB_KEY: &str = "7Lo0ffGCnHm+4JhskvB1Wbf1kDhVHFgYxZkGdx8c3Ns=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["116.213.40.155"];
+pub const PUBLIC_RS_PUB_KEY: &str = "TSCCEvYtQIkaKbXVn8BNMJHB87RKwlEs4BN14klRspE=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
